1886203514bb435f4c7d4b754b64bbfe100380c6
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-03-06 20:05:32 -0600

    A premium user's private wikis become public when she downgrades

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
   end
 
   def downgrade
+      private_wikis_to_convert = Wiki.where(user_id: current_user.id, private: true)
+      private_wikis_to_convert.each do |item|
+          item.private = false
+          item.save
+      end
       current_user.standard!
       flash[:sucess] = "Your account has been downgraded!"
       redirect_to request.referrer
