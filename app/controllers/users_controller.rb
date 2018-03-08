8ba6bcd005564f54e73c82b022bef3da2e92bb93
Marco Lopez-Silva <mlopez@fundacionidea.org.mx>
2018-03-07 21:54:34 -0600

    Third suggestion by grader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def downgrade
-      Wiki.where(user_id: current_user.id, private: true).update_all(private: false)
+      #Wiki.where(user_id: current_user.id, private: true).update_all(private: false)
       current_user.standard!
       flash[:sucess] = "Your account has been downgraded!"
       redirect_to request.referrer
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :wikis
 
   after_initialize :init
+  after_update :check_private
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -12,8 +13,16 @@ class User < ApplicationRecord
   #NL enables user.role, user.admin?, user.premium?, user.standard? (and current_user.admin? etc)
   enum role: [:standard, :premium, :admin]
 
+private
+
   def init
     self.role ||= :standard
+  end
+
+  def check_private
+    if self.standard?
+      Wiki.where(user_id: self.id, private: true).update_all(private: false)
+    end
   end
 
 end
